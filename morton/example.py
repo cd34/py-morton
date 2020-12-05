2a0e802a923a366bf40d7e1e32c9351ce0a2342b
Chris Davies <c.gitlab@cd34.com>
2020-12-05 07:03:25 +0000

    update for python3

--- morton/example.py
+++ morton/example.py
@@ -5,10 +5,10 @@ from morton import (
     get_morton,
     )
 
-print get_morton(1,1)
-print get_morton(1,2)
-print get_morton(2,1)
-print get_morton(2,2)
+print(get_morton(1,1))
+print(get_morton(1,2))
+print(get_morton(2,1))
+print(get_morton(2,2))
 
-print get_latlong_morton(26.6615, -80.26)
-print get_latlong_morton(26.679, -80.2037)
+print(get_latlong_morton(26.6615, -80.26))
+print(get_latlong_morton(26.679, -80.2037))
